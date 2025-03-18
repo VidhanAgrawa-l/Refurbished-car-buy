6bbd1279ac1d0df635c8f0a9953d96b04a85e011
VidhanAgrawa-l <vidhanagrawal786@gmail.com>
2025-03-18 14:09:07 +0530

    data ingestion set

--- refurbished_car/components/data_ingestion.py
+++ refurbished_car/components/data_ingestion.py
@@ -16,13 +16,13 @@ from sklearn.model_selection import train_test_split
 from dotenv import load_dotenv
 load_dotenv()
 
-MONGO_DB_URL=os.getenv("MONGO_DB_URL")
+MONGO_DB_URL=os.getenv("MONGO_DB_URL") # reading the mongo db url from the environment variable 
 
 
 class DataIngestion:
-    def __init__(self,data_ingestion_config:DataIngestionConfig):
+    def __init__(self,data_ingestion_config:DataIngestionConfig):# inherit the DataIngestionConfig class from the config_entity.py file
         try:
-            self.data_ingestion_config=data_ingestion_config
+            self.data_ingestion_config=data_ingestion_config# this will have all the configuration related to the data ingestion since we are inheriting the DataIngestionConfig class
         except Exception as e:
             raise RefurbishedCarException(e,sys)
         
@@ -31,23 +31,26 @@ class DataIngestion:
         Read data from mongodb
         """
         try:
-            database_name=self.data_ingestion_config.database_name
-            collection_name=self.data_ingestion_config.collection_name
+            database_name=self.data_ingestion_config.database_name # reading the database name from the data_ingestion_config WHICH IS INHERITED FROM THE DataIngestionConfig class of config_entity.py file
+            collection_name=self.data_ingestion_config.collection_name # reading the collection name from the data_ingestion_config WHICH IS INHERITED FROM THE DataIngestionConfig class of config_entity.py file
             self.mongo_client=pymongo.MongoClient(MONGO_DB_URL)
-            collection=self.mongo_client[database_name][collection_name]
+            collection=self.mongo_client[database_name][collection_name]# reading the collection from the database which is present in the mongodb
 
-            df=pd.DataFrame(list(collection.find()))
-            if "_id" in df.columns.to_list():
-                df=df.drop(columns=["_id"],axis=1)
+            df=pd.DataFrame(list(collection.find()))# reading the data from the collection and converting it into the dataframe
+            if "_id" in df.columns.to_list():# checking if the _id column is present in the dataframe
+                df=df.drop(columns=["_id"],axis=1)# if present then drop the column
             
-            df.replace({"na":np.nan},inplace=True)
+            df.replace({"na":np.nan},inplace=True) # replace the na values with the np.nan values
             return df
         except Exception as e:
             raise RefurbishedCarException
         
     def export_data_into_feature_store(self,dataframe: pd.DataFrame):
+        """
+        Export the data into the feature store file path as csv from mongodb
+        """
         try:
-            feature_store_file_path=self.data_ingestion_config.feature_store_file_path
+            feature_store_file_path=self.data_ingestion_config.feature_store_file_path 
             #creating folder
             dir_path = os.path.dirname(feature_store_file_path)
             os.makedirs(dir_path,exist_ok=True)
